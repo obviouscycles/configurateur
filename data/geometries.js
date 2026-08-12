b73863a7019916e92a1152bdf883f5ab29c2487f
obviouscycles <info@obviouscycles.com>
2026-08-12 15:14:56 +0200

    Add files via upload

--- data/geometries.js
+++ data/geometries.js
@@ -6,43 +6,43 @@ const TAILLES_CADRE = {
       "taille": "XXS",
       "stature_min": 155,
       "stature_max": 164,
-      "ej_min": 73.9,
-      "ej_max": 78.4
+      "ej_min": 70,
+      "ej_max": 81.4
     },
     {
       "taille": "XS",
       "stature_min": 163,
       "stature_max": 171,
-      "ej_min": 77.3,
-      "ej_max": 81.8
+      "ej_min": 73.4,
+      "ej_max": 84.8
     },
     {
       "taille": "S",
       "stature_min": 170,
       "stature_max": 177,
-      "ej_min": 80.7,
-      "ej_max": 85.2
+      "ej_min": 76.8,
+      "ej_max": 88.2
     },
     {
       "taille": "M",
       "stature_min": 176,
       "stature_max": 182,
-      "ej_min": 84.1,
-      "ej_max": 88.6
+      "ej_min": 80.2,
+      "ej_max": 91.6
     },
     {
       "taille": "L",
       "stature_min": 181,
       "stature_max": 187,
-      "ej_min": 87.5,
-      "ej_max": 92.0
+      "ej_min": 83.6,
+      "ej_max": 95
     },
     {
       "taille": "XL",
       "stature_min": 186,
       "stature_max": 194,
-      "ej_min": 90.9,
-      "ej_max": 95.5
+      "ej_min": 87,
+      "ej_max": 98.4
     }
   ],
   "gravel_racing": [
@@ -50,36 +50,36 @@ const TAILLES_CADRE = {
       "taille": "XS",
       "stature_min": 160,
       "stature_max": 168,
-      "ej_min": 76.3,
-      "ej_max": 80.9
+      "ej_min": 72.8,
+      "ej_max": 83.1
     },
     {
       "taille": "S",
       "stature_min": 166,
       "stature_max": 174,
-      "ej_min": 79.7,
-      "ej_max": 84.3
+      "ej_min": 76.2,
+      "ej_max": 86.5
     },
     {
       "taille": "M",
       "stature_min": 172,
       "stature_max": 180,
-      "ej_min": 83.1,
-      "ej_max": 87.7
+      "ej_min": 79.6,
+      "ej_max": 89.9
     },
     {
       "taille": "L",
       "stature_min": 178,
       "stature_max": 186,
-      "ej_min": 86.6,
-      "ej_max": 91.1
+      "ej_min": 83,
+      "ej_max": 93.3
     },
     {
       "taille": "XL",
       "stature_min": 184,
       "stature_max": 194,
-      "ej_min": 90.0,
-      "ej_max": 94.5
+      "ej_min": 86.4,
+      "ej_max": 96.7
     }
   ],
   "gravel_bikepacking": [
@@ -87,36 +87,36 @@ const TAILLES_CADRE = {
       "taille": "XS",
       "stature_min": 160,
       "stature_max": 168,
-      "ej_min": 75.3,
-      "ej_max": 79.9
+      "ej_min": 71.7,
+      "ej_max": 82
     },
     {
       "taille": "S",
       "stature_min": 166,
       "stature_max": 174,
-      "ej_min": 78.8,
-      "ej_max": 83.3
+      "ej_min": 75.1,
+      "ej_max": 85.4
     },
     {
       "taille": "M",
       "stature_min": 172,
       "stature_max": 180,
-      "ej_min": 82.2,
-      "ej_max": 86.8
+      "ej_min": 78.5,
+      "ej_max": 88.8
     },
     {
       "taille": "L",
       "stature_min": 178,
       "stature_max": 186,
-      "ej_min": 85.6,
-      "ej_max": 90.2
+      "ej_min": 81.9,
+      "ej_max": 92.1
     },
     {
       "taille": "XL",
       "stature_min": 184,
       "stature_max": 194,
-      "ej_min": 89.0,
-      "ej_max": 93.6
+      "ej_min": 85.3,
+      "ej_max": 95.5
     }
   ],
   "vtt_enduro": [
@@ -124,29 +124,29 @@ const TAILLES_CADRE = {
       "taille": "S",
       "stature_min": 164,
       "stature_max": 172,
-      "ej_min": 77.2,
-      "ej_max": 83.9
+      "ej_min": 74.2,
+      "ej_max": 86.7
     },
     {
       "taille": "M",
       "stature_min": 170,
       "stature_max": 178,
-      "ej_min": 83.2,
-      "ej_max": 89.0
+      "ej_min": 78.8,
+      "ej_max": 93.2
     },
     {
       "taille": "L",
       "stature_min": 176,
       "stature_max": 184,
-      "ej_min": 87.8,
-      "ej_max": 93.5
+      "ej_min": 83.4,
+      "ej_max": 97.8
     },
     {
       "taille": "XL",
       "stature_min": 182,
       "stature_max": 192,
-      "ej_min": 92.4,
-      "ej_max": 98.1
+      "ej_min": 88,
+      "ej_max": 102.3
     }
   ]
-}
+};
